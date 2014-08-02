a8a988b214760e4958a53e9d2b08144e1e104779
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-08-02 12:26:59 -0400

    Added method to remove all uploaded images when seeding the DB.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -13,8 +13,26 @@ class DatabaseSeeder extends Seeder {
 		$this->call('GroupSeeder');
 		$this->call('LanguageSeeder');
 		if ( (App::environment() == 'local') || (App::environment() == 'staging' ) ){
+			$this->deleteImages();
 			$this->call('SettingsSeeder');
 			$this->call('UserSeeder');
+		}
+	}
+
+	/**
+	* Delete all the uploaded images from social imports
+	*/
+	private function deleteImages()
+	{
+		$twitter_images = glob(public_path() . '/assets/uploads/twitter_images/*');
+		foreach($twitter_images as $file){
+			if(is_file($file))
+			unlink($file);
+		}
+		$instagram_images = glob(public_path() . '/assets/uploads/instagram_images/*');
+		foreach($instagram_images as $file){
+			if(is_file($file))
+			unlink($file);
 		}
 	}
 
