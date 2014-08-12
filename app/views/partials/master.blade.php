c605acc76b74411b8f8627b38c640910089276c0
Kyle Phillips <kyle@object9.com>
2014-08-12 16:47:12 -0400

    Page sep title and description added to master template.

--- app/views/partials/master.blade.php
+++ app/views/partials/master.blade.php
@@ -3,14 +3,18 @@
 <head>
 	<meta charset="UTF-8" />
 
-	<title>Kickapoo Joy Juice</title>
-	
+	<title>Kickapoo Joy Juice @if($page['seo_title'])- {{$page['seo_title']}} @endif</title>
+
+@if($page['seo_description'])
+	<meta name="description" content="{{$page['seo_description']}}">
+@endif
+
 	<link rel="icon" href="/favicon.ico">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	@if(Auth::check())
+@if(Auth::check())
 	<link rel="stylesheet" href="{{URL::asset('assets/css/admin/admin-styles.css')}}">
-	@endif
+@endif
 	
 	<link rel="stylesheet" href="{{URL::asset('assets/css/styles.css')}}">
 
@@ -22,7 +26,7 @@
 		<link rel="stylesheet" type="text/css" href="/assets/css/ie7.css">
 	<![endif]-->
 
-    <!--[if lt IE 9]>
+	<!--[if lt IE 9]>
     	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<script>window.html5 || document.write('<script src="/assets/js/html5shiv.js"><\/script>')</script>
 	<![endif]-->
