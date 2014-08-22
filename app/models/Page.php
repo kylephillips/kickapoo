e1e2b032a23f127f9e6b29d55236960fe44b7f7e
Kyle Phillips <kyle@object9.com>
2014-08-22 11:23:13 -0400

    Added method to Page model to get a specific custom field.

--- app/models/Page.php
+++ app/models/Page.php
@@ -7,9 +7,25 @@ class Page extends Eloquent {
 		'title', 'slug', 'content', 'author', 'status', 'menu_order', 'template', 'seo_title', 'seo_description', 'show_in_menu'
 	];
 
+	/**
+	* Get all the custom fields for this page
+	*/
 	public function customfields()
 	{
 		return $this->hasMany('CustomField');
+	}
+
+	/**
+	* Get a specified custom field - for use in templates
+	*/
+	public function get_field($field, $pageid)
+	{
+		try {
+			$field = CustomField::where('name',$field)->where('page_id',$pageid)->first();
+			return $field->value;
+		} catch (\Exception $e){
+			return false;
+		}
 	}
 	
 }
