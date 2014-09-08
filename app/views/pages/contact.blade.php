ca99e50d8e47edb92a305546cc68510c9ec8bf53
Kyle Phillips <kyle@object9.com>
2014-09-08 12:43:07 -0400

    Contact page template & styles

--- app/views/pages/contact.blade.php
+++ app/views/pages/contact.blade.php
@@ -1,40 +1,65 @@
 @extends('partials.master')
 @section('content')
-<h1>Contact Page</h1>
+	
+	<div class="container">
+		@if( $page->get_field('Header Image', $page->id) )
+			<img src="{{URL::asset('assets/uploads/page_images')}}/{{$page->get_field('Header Image', $page->id)}}" class="header-image" alt="{{$page['title']}}" />
+		@else
+			<h1>{{$page['title']}}</h1>
+		@endif
+	</div>
 
-@if(Session::has('success'))
-<div class="alert alert-success">{{Session::get('success')}}</div>
-@else
+	<hr class="bubble-pattern">
 
-{{Form::open(['url'=>URL::route('process_form')])}}
+	</section><!-- page-hero -->
 
-@if(Session::has('errors'))
-<div class="alert alert-danger">Ooops! Looks like there were some errors. That's not Joyful!</div>
-@endif
+	<section class="page-content">
+		<div class="container full contact-page">
 
-<p>
-	{{$errors->first('name', '<span class="text-danger"><strong>:message</strong></span><br>')}}
-	{{Form::label('name', 'Your Name')}}
-	{{Form::text('name')}}
-</p>
-<p>
-	{{$errors->first('email', '<span class="text-danger"><strong>:message</strong></span><br>')}}
-	{{Form::label('email', 'Your Email')}}
-	{{Form::email('email')}}
-</p>
-<p>
-	{{$errors->first('message', '<span class="text-danger"><strong>:message</strong></span><br>')}}
-	{{Form::label('message', 'Message')}}
-	{{Form::textarea('message')}}
-</p>
-<p>
-	{{$errors->first('user-captcha', '<span class="text-danger"><strong>:message</strong></span><br>')}}
-	<img src="{{ URL::to('/captcha')}}">
-	<input type="text" name="user-captcha">
-</p>
-{{Form::submit('Send')}}
-{{Form::close()}}
+			@if(Session::has('success'))
+				@if( $page->get_field('Success Message', $page->id) )
+				<div class="alert alert-success">{{$page->get_field('Success Message', $page->id)}}</div>
+				@else
+				<div class="alert alert-success">{{Session::get('success')}}</div>
+				@endif
+			@else
 
-@endif
+			@if($page->content)
+			{{$page->content}}
+			@endif
+
+			{{Form::open(['url'=>URL::route('process_form'), 'class'=>'contact-form'])}}
+
+				@if(Session::has('errors'))
+				<div class="alert alert-danger">Ooops! Looks like there were some errors. That's not Joyful!</div>
+				@endif
+
+				<p class="left">
+					{{$errors->first('name', '<span class="text-danger"><strong>:message</strong></span><br>')}}
+					{{Form::label('name', 'Your Name')}}
+					{{Form::text('name')}}
+				</p>
+				<p class="right">
+					{{$errors->first('email', '<span class="text-danger"><strong>:message</strong></span><br>')}}
+					{{Form::label('email', 'Your Email')}}
+					{{Form::email('email')}}
+				</p>
+				<p>
+					{{$errors->first('message', '<span class="text-danger"><strong>:message</strong></span><br>')}}
+					{{Form::label('message', 'Message')}}
+					{{Form::textarea('message')}}
+				</p>
+				{{$errors->first('user-captcha', '<span class="text-danger"><strong>:message</strong></span><br>')}}
+				<p class="captcha">
+					{{Form::label('user-captcha', 'Please enter the following')}}
+					<img src="{{ URL::to('/captcha')}}">
+					<input type="text" name="user-captcha">
+				</p>
+				{{Form::submit('Send')}}
+			{{Form::close()}}
+		</div><!-- .container -->
+	</section><!-- .page-content -->
+
+	@endif
 
 @stop
--- app/views/pages/default_page.blade.php
+++ app/views/pages/default_page.blade.php
@@ -1,5 +1,14 @@
 @extends('partials.master')
 @section('content')
-<h1>{{$page['title']}}</h1>
-<?php print_r($page); ?>
+<div class="container">
+		@if( $page->get_field('Header Image', $page->id) )
+			<img src="{{URL::asset('assets/uploads/page_images')}}/{{$page->get_field('Header Image', $page->id)}}" class="header-image" alt="{{$page['title']}}" />
+		@else
+			<h1>{{$page['title']}}</h1>
+		@endif
+	</div>
+
+	<hr class="bubble-pattern">
+
+	</section><!-- page-hero -->
 @stop
