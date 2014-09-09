0d61ee3926a5f280085ae660337199472dc9d194
Kyle Phillips <kyle@object9.com>
2014-09-09 17:25:20 -0400

    Mobile styles and image loading for products page

--- app/views/pages/locate.blade.php
+++ app/views/pages/locate.blade.php
@@ -27,8 +27,46 @@
 		</div>
 
 		@if( $page->get_field('Flavors Image', $page->id) )
-			<img src="{{URL::asset('assets/uploads/page_images')}}/{{$page->get_field('Flavors Image', $page->id)}}" class="flavors-image" alt="Kickapoo Flavors" />
+			<div class="large-image" data-image="{{$page->get_field('Flavors Image', $page->id)}}" data-title="Kickapoo Flavors"></div>
 		@endif
 	</section><!-- .locate-page -->
 
+@stop
+
+@section('footercontent')
+<script>
+/**
+* Only load large images on big screens
+*/
+$(document).ready(function(){
+	load_large_images();
+});
+
+$(window).resize(function(){
+	delay(function(){
+		load_large_images();
+	}, 100);
+});
+
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+function load_large_images()
+{
+	if ( $(window).width() > 767 ){
+		var images = $('.large-image');
+		$.each(images, function(i, v){
+			var image = $(this).data('image');
+			var title = $(this).data('title');
+			var html = '<img src="{{URL::asset('assets/uploads/page_images')}}/' + image + '" alt="Kickapoo Flavors" />';
+			$(this).html(html);
+		});
+	}
+}
+</script>
 @stop
