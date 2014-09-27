0ac808182a6daad2bb5b630e0597989a770ca9e0
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-09-27 18:16:23 -0400

    Admin style cleanup, footer made fixed.

--- app/views/admin/partials/footer.blade.php
+++ app/views/admin/partials/footer.blade.php
@@ -1,5 +1,4 @@
-<footer>
-	<div class="container small">
-		&copy; <?php echo date('Y'); ?> The Monarch Beverage Company, Inc. Crafted by your friends at <a href="http://object9.com" target="_blank">Object 9</a>.
-	</div>
+<footer class="admin">
+	&copy; <?php echo date('Y'); ?> The Monarch Beverage Company, Inc. 
+	<span>Crafted by your friends at <a href="http://object9.com" target="_blank">Object 9</a>.</span>
 </footer>
